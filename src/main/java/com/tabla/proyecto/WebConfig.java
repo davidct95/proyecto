5e92ca754750abc6523bd914a5ad9e299c974eb5
Davidct <97263791+davidct95@users.noreply.github.com>
2024-02-28 22:17:07 -0500

    Agregando cambios

--- src/main/java/com/tabla/proyecto/WebConfig.java
+++ src/main/java/com/tabla/proyecto/WebConfig.java
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-package com.tabla.proyecto;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
-}
-=======
 package com.tabla.proyecto;
 
 import org.springframework.context.annotation.Configuration;
@@ -51,4 +23,3 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
->>>>>>> cambio-interfaz
